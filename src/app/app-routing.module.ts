8e4845550eeb346cdce0e091310f47d575266dc1
sreekanth56 <koye.sree@gmail.com>
2020-03-17 11:34:24 +0530

    interval observablse

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
     component: HomeComponent
   },
   {
-    path:'user:/id',
+    path:'user/:id',
     component: UserComponent
   }
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit,OnDestroy {
+
+  private firstObsSebscription: Subscription;
 
   constructor() { }
 
   ngOnInit(): void {
+
+   this.firstObsSebscription = interval(1000).subscribe(count=>{
+      console.log(count);
+    });
   }
 
+  ngOnDestroy(): void{
+
+    this.firstObsSebscription.unsubscribe();
+
+}
 }
